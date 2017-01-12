5feb820756fa65e81926ce58932043c6088fded8
Stan Sorochan <stan.ua@gmail.com>
2017-01-11 22:56:39 -0500

    add sqlite as cookbook

---
+++ cookbooks/sqlite/metadata.rb
@@ -0,0 +1,16 @@
+name 'sqlite'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs sqlite'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.1.3'
+
+recipe 'sqlite::default', 'Installs sqlite'
+
+%w(ubuntu debian centos redhat scientific oracle amazon fedora).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/sqlite' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/sqlite/issues' if respond_to?(:issues_url)
